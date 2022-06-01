893248ffff281b0d102a5ecb6e66fb3329b4930d
Rafaela Toniatto <rafaela.toniatto@gmail.com>
2022-06-01 20:21:20 -0300

    Calculadora em C#

---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+
+//Programa de uma calculadora simples
+class Calculadora
+{
+    static void Main(string[] args)
+    {
+        double num1, num2;
+
+
+        Console.WriteLine("CALCULADORA SIMPLES \r\n");
+        Console.WriteLine("Digite o primeiro número:");
+        num1 = double.Parse(Console.ReadLine());
+
+
+        Console.WriteLine("Digite o segundo número:");
+        num2 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("\r\nO resultado da sua soma é {0}", num1 + num2);
+        Console.WriteLine("O resultado da sua substração é {0}", num1 - num2);
+        Console.WriteLine("O resultado da sua multiplicação é {0}", num1 * num2);
+        Console.WriteLine("O resultado da sua divisão é {0}", num1 / num2);
+
+        Console.ReadLine();
+        Console.ReadKey();
+
+    }
+}
